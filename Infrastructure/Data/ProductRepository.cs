4e5a79ec62b07fd2b8eb06de88e9e81d5c876418
Saketh <saketh.kalam@gmail.com>
2023-12-04 21:49:03 -0500

    2nd commit on 04Dec2023

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext context;
+        public ProductRepository(StoreContext context)
+        {
+            this.context = context;
+
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await context.Products.ToListAsync();
+        }
+    }
+}
